9dbd4152d55ed9918fd2b549ba28781699baa898
L-jac <3137501630@qq.com>
2023-04-14 12:09:41 +0800

    学习记录 第二章 处理文件，摄像头，GUI

--- chapter02/cameo/cameo.py
+++ chapter02/cameo/cameo.py
@@ -7,6 +7,8 @@ class Cameo(object):
     def __init__(self):
         self._windowManager = WindowManager('Cameo',
                                             self.onKeypress)
+        # 不能直接使用 onKeypress()，因为这样会立即调用 onKeypress 方法，
+        # 而不是将其作为参数传递给 WindowManager 类的构造函数。
         self._captureManager = CaptureManager(
             cv2.VideoCapture(0), self._windowManager, True)
 
@@ -15,6 +17,7 @@ class Cameo(object):
         self._windowManager.createWindow()
         while self._windowManager.isWindowCreated:
             self._captureManager.enterFrame()
+            # 这里用到了CaptureManager中的frame的getter方法
             frame = self._captureManager.frame
 
             if frame is not None:
@@ -27,9 +30,9 @@ class Cameo(object):
     def onKeypress(self, keycode):
         """Handle a keypress.
 
-        space  -> Take a screenshot.
-        tab    -> Start/stop recording a screencast.
-        escape -> Quit.
+        空格键 -> 拍摄屏幕截图。
+        Tab 键 -> 开始/停止录制屏幕。
+        Esc 键 -> 退出。
 
         """
         if keycode == 32:  # space
@@ -44,5 +47,6 @@ class Cameo(object):
             self._windowManager.destroyWindow()
 
 
+# 当你直接运行一个 Python 文件时，它的 __name__ 属性会被设置为 "__main__"。
 if __name__ == "__main__":
     Cameo().run()
--- chapter02/cameo/managers.py
+++ chapter02/cameo/managers.py
@@ -3,6 +3,9 @@ import numpy
 import time
 
 
+# 在Python 2中，class ClassName(object):是一种固定语法，用于定义一个新类，它表示这个类继承自object类。
+# object类是Python中所有新式类的基类，它提供了一些基本的方法和属性。
+# 在Python 3中，你可以省略(object)，直接使用class ClassName:来定义一个新类，因为在Python 3中所有的类都隐式地继承自object类。
 class CaptureManager(object):
 
     def __init__(self, capture, previewWindowManager=None,
@@ -25,10 +28,15 @@ class CaptureManager(object):
         self._fpsEstimate = None
 
     @property
+    # @property 是一个 Python 内置的装饰器，用于将类方法转换为只读属性
+    # channel方法被定义为一个属性。这意味着你可以使用 object.channel 来访问该方法的返回值，
+    # 而不是使用 object.channel()。
     def channel(self):
         return self._channel
 
     @channel.setter
+    # @property_name.setter 装饰器来定义一个 setter 方法，用于设置属性的值。
+    # 可以使用 object.channel = value 来设置新的通道值。
     def channel(self, value):
         if self._channel != value:
             self._channel = value
@@ -37,11 +45,15 @@ class CaptureManager(object):
     @property
     def frame(self):
         if self._enteredFrame and self._frame is None:
-            _, self._frame = self._capture.retrieve(
-                self._frame, self.channel)
+            # self._capture.retrieve 方法返回两个值，但只有第二个值（帧）是需要的。
+            # 因此，第一个返回值被赋值给 _，表示不关心该返回值。
+            # 这种写法可以让代码更简洁、易读。
+            _, self._frame = \
+                self._capture.retrieve(self._frame, self.channel)
         return self._frame
 
     @property
+    # 如果非空返回self._imageFilename
     def isWritingImage(self):
         return self._imageFilename is not None
 
@@ -55,10 +67,18 @@ class CaptureManager(object):
         """Capture the next frame, if any."""
 
         # But first, check that any previous frame was exited.
+        # assert 语句检查 self._enteredFrame 是否为 False。
+        # 如果为 True，则抛出一个异常，
+        # 异常信息为 'previous enterFrame() had no matching exitFrame()'。
         assert not self._enteredFrame, \
             'previous enterFrame() had no matching exitFrame()'
-
+        # 这种写法与使用 assert 语句的效果相同，但更加明确和易读。
+        # if self._enteredFrame:
+        #     raise Exception('previous enterFrame() had no matching exitFrame()')
         if self._capture is not None:
+            # _capture 是在创建 CaptureManager 类的实例时传入的一个参数。
+            # 因此，它可能是一个外部类的实例，具有 grab 方法。
+            # 调用grab()方法抓取帧
             self._enteredFrame = self._capture.grab()
 
     # TODO　实现从当前通道获取图像，估计帧率，显示图像(通过窗口)，完成图像写入文件的请求
@@ -67,11 +87,13 @@ class CaptureManager(object):
 
         # Check whether any grabbed frame is retrievable.
         # The getter may retrieve and cache the frame.
+        # 检查是否有可检索的帧。如果没有，则将 _enteredFrame 属性设置为 False 并返回。
         if self.frame is None:
             self._enteredFrame = False
             return
 
-        # Update the FPS estimate and related variables.
+        # 更新 FPS 估计值和相关变量。如果是第一帧，则记录开始时间。
+        # 否则，计算经过的时间并更新 FPS 估计值。
         if self._framesElapsed == 0:
             self._startTime = time.perf_counter()
         else:
@@ -79,23 +101,25 @@ class CaptureManager(object):
             self._fpsEstimate = self._framesElapsed / timeElapsed
         self._framesElapsed += 1
 
-        # Draw to the window, if any.
+        # 如果有预览窗口，则绘制窗口。
+        # 如果需要镜像预览，显示翻转后的帧。否则，直接显示原始帧。
         if self.previewWindowManager is not None:
-            if self.shouldMirrorPreview:
+            if self.shouldMirrorPreview:  # should mirror preview应该镜像预览
                 mirroredFrame = numpy.fliplr(self._frame)
                 self.previewWindowManager.show(mirroredFrame)
             else:
                 self.previewWindowManager.show(self._frame)
 
-        # Write to the image file, if any.
+        # 如果正在写入图像文件，则使用 cv2.imwrite 方法将帧写入文件，
+        # 并将 _imageFilename 属性设置为 None。
         if self.isWritingImage:
             cv2.imwrite(self._imageFilename, self._frame)
             self._imageFilename = None
 
-        # Write to the video file, if any.
+        # 调用 _writeVideoFrame 方法写入视频帧，方法在下面
         self._writeVideoFrame()
 
-        # Release the frame.
+        # 释放帧并将 _enteredFrame 属性设置为 False
         self._frame = None
         self._enteredFrame = False
 
@@ -125,7 +149,7 @@ class CaptureManager(object):
         if self._videoWriter is None:
             fps = self._capture.get(cv2.CAP_PROP_FPS)
             if numpy.isnan(fps) or fps <= 0.0:
-                # The capture's FPS is unknown so use an estimate.
+                # 如果 FPS 值未知或小于等于 0，则使用 FPS 估计值。
                 if self._framesElapsed < 20:
                     # Wait until more frames elapse so that the
                     # estimate is more stable.
@@ -139,7 +163,7 @@ class CaptureManager(object):
             self._videoWriter = cv2.VideoWriter(
                 self._videoFilename, self._videoEncoding,
                 fps, size)
-
+        # self._videoWriter 是一个 cv2.VideoWriter 对象，自带write()
         self._videoWriter.write(self._frame)
 
 
