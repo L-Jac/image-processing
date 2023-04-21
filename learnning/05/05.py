e03fc88c0c7c3fdffabd9d4990cbb9bceacd8e46
L-jac <3137501630@qq.com>
2023-04-21 12:12:52 +0800

    学习记录 第五章 人脸检测和识别完结

--- learnning/05/05.py
+++ learnning/05/05.py
@@ -4,4 +4,12 @@
 #   chapter05/face_detection_still.py
 
 # 5.4.2视频上进行人脸检测
+#   chapter05/face_detection_video.py
 
+# 5.4.3进行人脸识别
+#   chapter05/generate_training_faces.py
+#   chapter05/face_recognition_video.py
+
+# TODO 需要深度摄像头
+# 5.5红外线下换脸
+#   chapter05/cameo
