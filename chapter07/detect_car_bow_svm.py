84c5db0272f2a7f8b39cb97cf918236d5a7c3711
L-jac <3137501630@qq.com>
2023-04-22 21:17:50 +0800

    学习记录 第七章 建立自定义物体检测器 7.7

--- chapter07/detect_car_bow_svm.py
+++ chapter07/detect_car_bow_svm.py
@@ -2,67 +2,101 @@ import cv2
 import numpy as np
 import os
 
+"""
 if not os.path.isdir('CarData'):
     print('CarData folder not found. Please download and unzip '
           'https://github.com/gcr/arc-evaluator/raw/master/CarData.tar.gz '
           'into the same folder as this script.')
     exit(1)
+"""
 
+# 两个训练阶段：
+# 一个阶段用于BoW词表，将使用大量图像作为样本；
+# 一个阶段用于支持向量机，将使用大量BoW描述符向量作为样本。
+# 用于训练 BoW 词表的样本数量
 BOW_NUM_TRAINING_SAMPLES_PER_CLASS = 10
+# 用于训练SVM 的每个类别的样本数量
 SVM_NUM_TRAINING_SAMPLES_PER_CLASS = 110
 
-BOW_NUM_CLUSTERS = 40
-
+# 创建 SIFT 特征检测器对象
 sift = cv2.SIFT_create()
 
+# 使用 FLANN 算法的参数创建 FLANN 匹配器对象
 FLANN_INDEX_KDTREE = 1
 index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
 search_params = dict(checks=50)
 flann = cv2.FlannBasedMatcher(index_params, search_params)
 
+# BoW 词表中聚类的数量(k均值聚类)
+BOW_NUM_CLUSTERS = 40
+# 使用 BOWKMeansTrainer 类创建了一个 BoW KMeans 训练器对象
+# 初始化时必须指定聚类数
 bow_kmeans_trainer = cv2.BOWKMeansTrainer(BOW_NUM_CLUSTERS)
+# 使用 BOWImgDescriptorExtractor 类创建了一个 BoW 图像描述符提取器对象
+# 初始化时必须指定描述符提取器和描述符匹配器
 bow_extractor = cv2.BOWImgDescriptorExtractor(sift, flann)
 
+
 def get_pos_and_neg_paths(i):
-    pos_path = 'CarData/TrainImages/pos-%d.pgm' % (i+1)
-    neg_path = 'CarData/TrainImages/neg-%d.pgm' % (i+1)
+    # 根据该参数生成两个文件路径，分别表示正样本和负样本的图像文件
+    pos_path = f'CarData/TrainImages/pos-{i + 1}.pgm'
+    neg_path = f'CarData/TrainImages/neg-{i + 1}.pgm'
     return pos_path, neg_path
 
+
 def add_sample(path):
+    # 常规灰度读图
     img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    # 使用 SIFT 特征检测器检测图像中的关键点和描述符
     keypoints, descriptors = sift.detectAndCompute(img, None)
+    # 如果描述符不为空，则将其添加到 BoW KMeans 训练器中。
     if descriptors is not None:
         bow_kmeans_trainer.add(descriptors)
 
+
+# 添加训练样本:
 for i in range(BOW_NUM_TRAINING_SAMPLES_PER_CLASS):
     pos_path, neg_path = get_pos_and_neg_paths(i)
     add_sample(pos_path)
     add_sample(neg_path)
 
+# 调用词表训练器的cluster方法，执行k均值分类并返回词表
 voc = bow_kmeans_trainer.cluster()
+# 把这个词表分配给BoW描述符提取器
+# 在这一阶段，BoW描述符提取器拥有了从高斯差分（Difference of Gaussian，DoG）特征提取BoW描述符所需要的一切。
 bow_extractor.setVocabulary(voc)
 
+
 def extract_bow_descriptors(img):
+    # 接受图像并返回由BoW描述符提取器计算的描述符向量。
+    # 这涉及图像的DoG特征提取以及基于DoG特征的BoW描述符向量的计算
     features = sift.detect(img)
     return bow_extractor.compute(img, features)
+
 
 training_data = []
 training_labels = []
 for i in range(SVM_NUM_TRAINING_SAMPLES_PER_CLASS):
     pos_path, neg_path = get_pos_and_neg_paths(i)
+    # 常规灰度读图
     pos_img = cv2.imread(pos_path, cv2.IMREAD_GRAYSCALE)
+    # 接受图像并返回由BoW描述符提取器计算的描述符向量
     pos_descriptors = extract_bow_descriptors(pos_img)
     if pos_descriptors is not None:
         training_data.extend(pos_descriptors)
         training_labels.append(1)
+    # 常规灰度读图
     neg_img = cv2.imread(neg_path, cv2.IMREAD_GRAYSCALE)
+    # 接受图像并返回由BoW描述符提取器计算的描述符向量
     neg_descriptors = extract_bow_descriptors(neg_img)
     if neg_descriptors is not None:
         training_data.extend(neg_descriptors)
         training_labels.append(-1)
 
+# OpenCV提供了名为cv2.ml_SVM的类，代表支持向量机。
+# 我们创建一个支持向量机，并用之前组建的数据和标签对其进行训练
 svm = cv2.ml.SVM_create()
-
+# 把训练数据和标签从列表转换为NumPy数组，然后再将它们传递给cv2.ml_SVM的train方法
 svm.train(np.array(training_data), cv2.ml.ROW_SAMPLE,
           np.array(training_labels))
 
@@ -72,9 +106,13 @@ for test_img_path in ['CarData/TestImages/test-0.pgm',
                       '../images/haying.jpg',
                       '../images/statue.jpg',
                       '../images/woodcutters.jpg']:
+    # 常规灰度读图
     img = cv2.imread(test_img_path)
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # 接受图像并返回由BoW描述符提取器计算的描述符向量
     descriptors = extract_bow_descriptors(gray_img)
+    # prediction变量是SVM模型的预测结果，一个包含两个元素的元组。
+    # 第一个元素是预测类别，第二个元素是一个包含概率估计的数组。
     prediction = svm.predict(descriptors)
     if prediction[1][0][0] == 1.0:
         text = 'car'
--- chapter07/detect_people_hog.py
+++ chapter07/detect_people_hog.py
@@ -1,34 +1,60 @@
 import cv2
 
-OPENCV_MAJOR_VERSION = int(cv2.__version__.split('.')[0])
-OPENCV_MINOR_VERSION = int(cv2.__version__.split('.')[1])
+
 
 def is_inside(i, o):
+    # i表示内部矩形，o表示外部矩形
+    # 如果i在o的内部返回true
     ix, iy, iw, ih = i
     ox, oy, ow, oh = o
     return ix > ox and ix + iw < ox + ow and \
         iy > oy and iy + ih < oy + oh
 
+
+# 创建cv2.HOGDescriptor实例
 hog = cv2.HOGDescriptor()
+# 使用OpenCV内置的默认人员检测器
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 img = cv2.imread('../images/haying.jpg')
 
-if OPENCV_MAJOR_VERSION >= 5 or \
-        (OPENCV_MAJOR_VERSION == 4 and OPENCV_MINOR_VERSION >= 6):
-    # OpenCV 4.6 or a later version is being used.
+# detectMultiScale方法，它返回两个列表：
+# （1）检测到的物体（在本例中是检测到的人）的矩形框列表。
+# （2）检测到的物体的权重或者置信度列表。值越高表示检测结果正确的置信度也就越大。
+found_rects, found_weights = hog.detectMultiScale(
+        img, winStride=(4, 4), scale=1.02, groupThreshold=1.9)
+# detectMultiScale接受几个可选参数，包括：
+# ·winStride：这个元组定义了滑动窗口在连续的检测尝试之间移动的x和y距离。
+#   HOG可以很好地处理重叠窗口，因此相对于窗口大小，步长可能较小。
+#   步长越小，检测次数越多，计算成本也越高。
+#   默认步长是让窗口无重叠，也就是与窗口大小相同，即(64,128)，用于默认的人员检测器。
+# ·scale：该尺度因子应用于图像金字塔的连续层之间。尺度因子越小，检测次数越多，
+#   计算成本也会越高。尺度因子必须大于1.0，默认值是1.5。
+# ·finalThreshold：这个值决定检测标准的严格程度。
+#   值越小，严格程度越低，检测次数越多。默认值是2.0。
+
+# 检测opencv的版本，4.6为区分
+"""
+OPENCV_MAJOR_VERSION = int(cv2.__version__.split('.')[0])
+OPENCV_MINOR_VERSION = int(cv2.__version__.split('.')[1])
+
+if OPENCV_MAJOR_VERSION >= 5 or (OPENCV_MAJOR_VERSION == 4 and OPENCV_MINOR_VERSION >= 6):
+    # 正在使用 OpenCV 4.6 或更高版本。
     found_rects, found_weights = hog.detectMultiScale(
         img, winStride=(4, 4), scale=1.02, groupThreshold=1.9)
 else:
-    # OpenCV 4.5 or an earlier version is being used.
-    # The groupThreshold parameter used to be named finalThreshold.
+    # 正在使用 OpenCV 4.5 或更早版本。
+    # groupThreshold 参数曾经被命名为 finalThreshold。
     found_rects, found_weights = hog.detectMultiScale(
         img, winStride=(4, 4), scale=1.02, finalThreshold=1.9)
+        """
 
+# 对检测结果进行过滤
 found_rects_filtered = []
 found_weights_filtered = []
 for ri, r in enumerate(found_rects):
     for qi, q in enumerate(found_rects):
+        # 去掉嵌套矩形
         if ri != qi and is_inside(r, q):
             break
     else:
@@ -38,7 +64,7 @@ for ri, r in enumerate(found_rects):
 for ri, r in enumerate(found_rects_filtered):
     x, y, w, h = r
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
-    text = '%.2f' % found_weights_filtered[ri]
+    text = f'{found_weights_filtered[ri]:.2f}'
     cv2.putText(img, text, (x, y - 20),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
 
